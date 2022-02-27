08c6bb44778f665fb47d9255860018498ce6bd63
javad aghajanloo <95215802+V3il3DOraCl3@users.noreply.github.com>
2022-02-27 19:04:34 +0330

    Add files via upload

---
+++ Assignments.py
@@ -0,0 +1,56 @@
+""" 6. Given the following assignment: """
+print("EXERCISES-6: ")
+x = 2
+print("x")
+print('x')
+print(x)
+print("x + 1")
+# print('x' + 1)
+print(x + 1)
+""" 7. Given the following assignments: """
+print("EXERCISES-7: ")
+i1 = 2
+i2 = 5
+i3 = -1
+d1 = 2.0
+d2 = 5.0
+d3 = -0.5
+print(i1 + i2)
+print(i1/i2)
+print(i1 // i2)
+print(i2 / i1)
+print(i2 // i1)
+print(i1 * i3)
+print(d1 + d2)
+print(d1 / d2)
+print(d2/d1)
+print(d3 * d1)
+print(d1 + i2)
+print(i1 / d2)
+print(d2 / i2)
+print(i1/i2*d1)
+print(d1*i1/i2)
+print(d1/d2*i1)
+print(i1*d1/d2)
+print(i2/i1*d1)
+print(d1*i2/i1)
+print(d2/d1*i1)
+print(i1*d2/d1)
+""" 9. Given the following assignments: """
+print("EXERCISES-9: ")
+print(i1 + (i2 * i3))
+print(i1 * (i2 + i3))
+print(i1 / (i2 + i3))
+print(i1 // (i2 + i3))
+print(i1 / i2 + i3)
+print(i1 // i2 + i3)
+print(3 + 4 + 5 / 3)
+print(3 + 4 + 5 // 3)
+print((3 + 4 + 5) / 3)
+print((3 + 4 + 5) // 3)
+print(d1 + (d2 * d3))
+print(d1 + d2 * d3)
+print(d1 / d2 - d3)
+print(d1 / (d2 - d3))
+print(d1 + d2 + d3 / 3)
+print(3 * (d1 + d2) * (d1 - d3))
